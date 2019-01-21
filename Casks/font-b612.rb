f60f870defff117c3589a34a5d17225e5f5bd44a
Caius Durling <dev@caius.name>
2019-01-21 19:54:13 +0000

    Install v1003 of B612 fonts

--- Casks/font-b612.rb
+++ Casks/font-b612.rb
@@ -1,18 +1,20 @@
-cask 'font-b612' do
-  version :latest
-  sha256 :no_check
+cask "font-b612" do
+  version "1003"
+  sha256 "53147cad9ff4d67deec6a0f87b5481d36b1e1a988063b47bc212be1a7cbce670"
 
-  url 'http://git.polarsys.org/c/b612/b612.git/snapshot/b612-master.zip'
-  name 'B612'
-  homepage 'http://b612-font.com'
+  git_sha = "bd14fde2544566e620eab106eb8d6f2b7fb1347e"
 
-  font 'B612 Mono-Bold.ttf'
-  font 'B612 Mono-BoldItalic.ttf'
-  font 'B612 Mono-Italic.ttf'
-  font 'B612 Mono-Regular.ttf'
+  url "http://git.polarsys.org/c/b612/b612.git/snapshot/b612-#{git_sha}.zip"
+  name "B612"
+  homepage "http://b612-font.com"
 
-  font 'B612-Bold.ttf'
-  font 'B612-BoldItalic.ttf'
-  font 'B612-Italic.ttf'
-  font 'B612-Regular.ttf'
+  font "b612-#{git_sha}/TTF/B612 Mono-Bold.ttf"
+  font "b612-#{git_sha}/TTF/B612 Mono-BoldItalic.ttf"
+  font "b612-#{git_sha}/TTF/B612 Mono-Italic.ttf"
+  font "b612-#{git_sha}/TTF/B612 Mono-Regular.ttf"
+
+  font "b612-#{git_sha}/TTF/B612-Bold.ttf"
+  font "b612-#{git_sha}/TTF/B612-BoldItalic.ttf"
+  font "b612-#{git_sha}/TTF/B612-Italic.ttf"
+  font "b612-#{git_sha}/TTF/B612-Regular.ttf"
 end
